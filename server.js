f6a5bde4d88a0c586d312f4d1c794d0e38c9fe52
angelicavc7 <angelicavillar_c@live.com>
2021-06-05 22:51:43 -0700

    readme

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
@@ -17,7 +17,7 @@ const hbs = exphbs.create({ helpers });
 const sess = {
   secret: 'Super secret secret',
   cookie: {
-    maxAge: 1000*60*60*2  //set to log out automatically after 2 hours.
+    maxAge: 1000*60*60*2  
   },
   resave: false,
   saveUninitialized: true,
